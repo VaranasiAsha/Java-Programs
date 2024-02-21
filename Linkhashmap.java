3806a6883d332350a75554835cbf9b697560693b
VaranasiAsha <155607344+VaranasiAsha@users.noreply.github.com>
2024-02-21 20:28:25 +0530

    Add files via upload

---
+++ Linkhashmap.java
@@ -0,0 +1,16 @@
+package asha;
+
+//import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+//import java.util.TreeMap;
+public class Linkedmap {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Map<Integer,String>m=new LinkedHashMap<Integer,String>();
+m.put(1,"a");
+m.put(3,"b");
+m.put(2,"d");
+System.out.println(m);
+	}
+}
