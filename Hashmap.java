8c397632b27dae6905d0afad7bddde3c53d70c0f
VaranasiAsha <155607344+VaranasiAsha@users.noreply.github.com>
2024-02-21 20:22:00 +0530

    Add files via upload

---
+++ Hashmap.java
@@ -0,0 +1,17 @@
+package asha;
+
+
+
+		import java.util.HashMap;
+ public class Hashmap {
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		HashMap<Integer,String>m=new HashMap<Integer,String>();
+		m.put(1,"a");
+		m.put(2,"b");
+		m.put(3,"d");
+		System.out.println(m);
+			}
+		}
+
+
