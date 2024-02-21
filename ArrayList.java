62549fbe5990f324cb289fc3fc1e5c3f63877e34
VaranasiAsha <155607344+VaranasiAsha@users.noreply.github.com>
2024-02-21 20:00:15 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,23 @@
+package asha;
+
+public class ArrayList {
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		ArrayList a1=new ArrayList();
+		a1.add(10);
+		a1.add(20);
+		a1.add(40);
+		a1.add(30);
+		System.out.println(a1);
+			}
+
+			private void add(int i) {
+				// TODO Auto-generated method stub
+				
+			}
+				
+			}
+		
+
+
+
