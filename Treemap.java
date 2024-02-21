b0634829701c965cd1fd6f6f14ccf561a51ac09a
VaranasiAsha <155607344+VaranasiAsha@users.noreply.github.com>
2024-02-21 20:09:21 +0530

    Add files via upload

---
+++ Treemap.java
@@ -0,0 +1,18 @@
+package asha;
+
+		import java.util.Map;
+		import java.util.TreeMap;
+		public class Treemap{
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		Map<Integer,String>m=new TreeMap<Integer,String>();
+		m.put(1,"a");
+		m.put(3,"b");
+		m.put(2,"d");
+		System.out.println(m);
+			}
+		
+
+	
+
+}
